f565b3e7b0df6fffa65bffb433fd49f6162cabac
Roman <romka200211@gmail.com>
2023-03-30 00:38:50 +0200

    first commit

---
+++ Задача 1. Знакомство с Visual Studio 2022.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, b;
+	std::ifstream file("in.txt");
+	std::ofstream fin("out.txt");
+	if (file.is_open())
+	{
+		file >> a;
+
+		int* mas = new int[a];
+
+		for (int i = 0; i < a;i++)
+		{
+			file >> mas[i];
+		}
+		for (int i = 0;i < a - 1;i++)
+		{
+			std::swap(mas[i], mas[i + 1]);
+		}
+
+		file >> b;
+
+		int* mas_2 = new int[b];
+
+		for (int i = 0; i < b;i++)
+		{
+			file >> mas_2[i];
+		}
+
+		for (int i = b - 1;i > 0;i--)
+		{
+			std::swap(mas_2[i], mas_2[i - 1]);
+		}
+
+		fin << b << std::endl;
+
+		for (int i = 0; i < b;i++)
+		{
+			fin << mas_2[i] << " ";
+		}
+
+		fin << std::endl;
+		fin << a << std::endl;
+
+		for (int i = 0; i < a;i++)
+		{
+			fin << mas[i] << " ";
+		}
+
+		delete[] mas;
+		delete[] mas_2;
+
+		file.close();
+		fin.close();
+	}
+	else
+	{
+		std::cout << "Не удалось открыть файл!";
+	}
+
+}
